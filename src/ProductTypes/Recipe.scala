a2c639cfde67944bea226c2e0e618d4ed892e141
ygtron <truongph@buffalo.edu>
2019-11-02 17:52:09 -0400

    Recipe update a roonie

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -3,9 +3,9 @@ import com.ibm.cloud.sdk.core.security.IamAuthenticator
 import com.ibm.watson.discovery.v1.Discovery
 import com.ibm.watson.discovery.v1.model._
 import com.ibm.cloud.sdk.core.http.HttpMediaType
-import java.io.ByteArrayInputStream
-import java.io.InputStream
 import Users._
+
+import scala.collection.mutable
 
 class Recipe(User: User) {
   val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
@@ -14,31 +14,28 @@ class Recipe(User: User) {
   val discovery = new Discovery("2019-04-30", authenticator)
   discovery.setServiceUrl(site)
 
+  //environment builder
   val environmentName = "User"
   val environmentDesc = "Customer/Employee"
-
   val createOptionsBuilder = new CreateEnvironmentOptions.Builder(environmentName)
   createOptionsBuilder.description(environmentDesc)
-  val createResponse: Environment = discovery.createEnvironment(createOptionsBuilder.build).execute.getResult
+  val environ: Environment = discovery.createEnvironment(createOptionsBuilder.build).execute.getResult
+
+  //collections builder
+  val createCollectionBuilder= new CreateCollectionOptions.Builder(environ.getEnvironmentId, "Main")
+  val collect: Collection = discovery.createCollection(createCollectionBuilder.build).execute.getResult
 
 //gets wegmans api
+  val url = "https://api.wegmans.io/products/175410?api-version=2018-10-18&Subscription-Key=da8f3095e1e94773add7ab4cb71eacc1"
+  var data: String = scala.io.Source.fromURL(url).mkString
 
-//  val environmentId = "{environment_id}"
-//  val collectionId = "{collection_id}"
-//  val documentId = "{document_id}"
-//  val documentJson = "{\"field\":\"value\"}"
-//  val documentStream = new ByteArrayInputStream(documentJson.getBytes)
+  val builder = new AddDocumentOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
+  builder.fileContentType(data)
+  builder.fileContentType(HttpMediaType.APPLICATION_JSON)
+  val response: DocumentAccepted = discovery.addDocument(builder.build).execute.getResult
 
-  //gets document data
-
-//  val builder = new AddDocumentOptions.Builder(environmentId, collectionId)
-//  builder.file(documentStream)
-//  builder.fileContentType(HttpMediaType.APPLICATION_JSON)
-//  val response: DocumentAccepted = discovery.addDocument(builder.build).execute.getResult
-
-
-
-  val queryBuilder = new QueryOptions.Builder(environmentId, collectionId)
+  //query builder
+  val queryBuilder = new QueryOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
   queryBuilder.query("{field}:{value}")
   val queryResponse: QueryResponse = discovery.query(queryBuilder.build).execute.getResult
 }
