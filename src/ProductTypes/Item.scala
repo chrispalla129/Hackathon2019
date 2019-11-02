e566f3e74eba3f8c985b52b2926e0a2723ee76c4
jbreindl <jbreindl@buffalo.edu>
2019-11-02 16:04:51 -0400

    Filling out Items and Shopping

--- src/ProductTypes/Item.scala
+++ src/ProductTypes/Item.scala
@@ -1,5 +1,11 @@
 package ProductTypes
 
 class Item {
+  val name: String = new String
 
+  val price: Double = new Double
+
+  val ingredients: List[String] = List()
+
+  val nutrients: List[Map[String, String]] = List()
 }
--- src/ProductTypes/ShoppingList.scala
+++ src/ProductTypes/ShoppingList.scala
@@ -2,4 +2,15 @@ package ProductTypes
 
 class ShoppingList {
 
+  var recipes: List[Recipe] = List()
+
+  var items: List[Item] = List()
+
+  val cost: Int = 0 // sum of all cost
+  def addRecipe(recipe: Recipe): Unit = {
+    recipes :+= recipe
+
+  }
+
+  def addItem (item: Item): Unit = items :+= item
 }
