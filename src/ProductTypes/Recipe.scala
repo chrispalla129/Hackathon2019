57263a3b5b4b0df1b95b0379d572bd78aa0a5ae0
ygtron <truongph@buffalo.edu>
2019-11-02 18:44:47 -0400

    Recipe update a roonie

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -8,35 +8,37 @@ import Users._
 import scala.collection.mutable
 
 class Recipe(User: User) {
-  val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
-  val site: String = "https://gateway.watsonplatform.net/discovery/api"
-  val authenticator = new IamAuthenticator(key)
-  val discovery = new Discovery("2019-04-30", authenticator)
+  val ingredients: List[List[Item]] = List()
+  private val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
+  private val site: String = "https://gateway.watsonplatform.net/discovery/api"
+  private val authenticator = new IamAuthenticator(key)
+  private val discovery = new Discovery("2019-04-30", authenticator)
   discovery.setServiceUrl(site)
 
   //environment builder
-  val environmentName = "User"
-  val environmentDesc = "Customer/Employee"
-  val createOptionsBuilder = new CreateEnvironmentOptions.Builder(environmentName)
+  private val environmentName = "User"
+  private val environmentDesc = "Customer/Employee"
+  private val createOptionsBuilder = new CreateEnvironmentOptions.Builder(environmentName)
   createOptionsBuilder.description(environmentDesc)
-  val environ: Environment = discovery.createEnvironment(createOptionsBuilder.build).execute.getResult
+  private val environ: Environment = discovery.createEnvironment(createOptionsBuilder.build).execute.getResult
 
   //collections builder
-  val createCollectionBuilder= new CreateCollectionOptions.Builder(environ.getEnvironmentId, "Main")
-  val collect: Collection = discovery.createCollection(createCollectionBuilder.build).execute.getResult
+  private val createCollectionBuilder= new CreateCollectionOptions.Builder(environ.getEnvironmentId, "Main")
+  private val collect: Collection = discovery.createCollection(createCollectionBuilder.build).execute.getResult
 
-//gets wegmans api
-  val url = "https://api.wegmans.io/products/175410?api-version=2018-10-18&Subscription-Key=da8f3095e1e94773add7ab4cb71eacc1"
-  var data: String = scala.io.Source.fromURL(url).mkString
+  //gets wegmans api
+  private val url = "https://api.wegmans.io/meals/recipes?api-version=2018-10-18&Subscription-Key=da8f3095e1e94773add7ab4cb71eacc1"
+  private val data: String = scala.io.Source.fromURL(url).mkString
 
-  val builder = new AddDocumentOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
+  private val builder = new AddDocumentOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
   builder.fileContentType(data)
   builder.fileContentType(HttpMediaType.APPLICATION_JSON)
-  val response: DocumentAccepted = discovery.addDocument(builder.build).execute.getResult
+  private val response: DocumentAccepted = discovery.addDocument(builder.build).execute.getResult
 
   //query builder
-  val queryBuilder = new QueryOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
-  queryBuilder.query("{field}:{value}")
+  private val queryBuilder = new QueryOptions.Builder(environ.getEnvironmentId, collect.getCollectionId)
+  private val recipename = "Spaghetti"
+  queryBuilder.query("{name:{Spaghetti}")
   val queryResponse: QueryResponse = discovery.query(queryBuilder.build).execute.getResult
 
 }
