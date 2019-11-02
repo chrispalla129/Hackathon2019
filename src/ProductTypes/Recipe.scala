a30716fdea139edf59f706c82b271229f2c7a156
ygtron <truongph@buffalo.edu>
2019-11-02 16:07:32 -0400

    Maven file updated to include the Watson

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -1,5 +1,6 @@
 package ProductTypes
 
 class Recipe {
-
+  val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
+  val site: String = "https://gateway.watsonplatform.net/discovery/api"
 }
