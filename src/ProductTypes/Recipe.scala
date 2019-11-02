ee907c2c7d95add3a0a796ba80d8092a841a4508
ygtron <truongph@buffalo.edu>
2019-11-02 16:30:52 -0400

    Maven file updated to include the Watson

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -1,6 +1,14 @@
 package ProductTypes
+import com.ibm.cloud.sdk.core.security.IamAuthenticator
+import com.ibm.watson.discovery.v1.Discovery
+
+import com.ibm.watson.discovery.v1.Discovery
 
 class Recipe {
-  val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
-  val site: String = "https://gateway.watsonplatform.net/discovery/api"
+
+
+
+  val authenticator = new IamAuthenticator("{apikey}")
+  val discovery = new Discovery("{version}", authenticator)
+  discovery.setServiceUrl("{url}")
 }
