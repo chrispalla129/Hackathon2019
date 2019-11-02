133f25e6508e99b6776b2dd0cb1e86dfb61b6e85
ygtron <truongph@buffalo.edu>
2019-11-02 17:01:04 -0400

    Recipe update

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -1,6 +1,13 @@
 package ProductTypes
 import com.ibm.cloud.sdk.core.security.IamAuthenticator
 import com.ibm.watson.discovery.v1.Discovery
+import com.ibm.watson.discovery.v1.model.CreateEnvironmentOptions
+import com.ibm.watson.discovery.v1.model.Environment
+import com.ibm.cloud.sdk.core.http.HttpMediaType
+import com.ibm.watson.discovery.v1.model.AddDocumentOptions
+import com.ibm.watson.discovery.v1.model.DocumentAccepted
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
 class Recipe {
   val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
@@ -8,4 +15,26 @@ class Recipe {
   val authenticator = new IamAuthenticator(key)
   val discovery = new Discovery("2019-04-30", authenticator)
   discovery.setServiceUrl(site)
+
+  val environmentName = "User"
+  val environmentDesc = "Customer/Employee"
+
+  val createOptionsBuilder = new CreateEnvironmentOptions.Builder(environmentName)
+  createOptionsBuilder.description(environmentDesc)
+  val createResponse: Environment = discovery.createEnvironment(createOptionsBuilder.build).execute.getResult
+
+//gets wegmans api
+
+//  val environmentId = "{environment_id}"
+//  val collectionId = "{collection_id}"
+//  val documentId = "{document_id}"
+//  val documentJson = "{\"field\":\"value\"}"
+//  val documentStream = new ByteArrayInputStream(documentJson.getBytes)
+
+  //gets document data
+
+//  val builder = new AddDocumentOptions.Builder(environmentId, collectionId)
+//  builder.file(documentStream)
+//  builder.fileContentType(HttpMediaType.APPLICATION_JSON)
+//  val response: DocumentAccepted = discovery.addDocument(builder.build).execute.getResult
 }
