c20218598672cd7131070ba90b920669094d0326
jbreindl <jbreindl@buffalo.edu>
2019-11-02 18:15:15 -0400

    vars

--- src/ProductTypes/Item.scala
+++ src/ProductTypes/Item.scala
@@ -1,22 +1,21 @@
 package ProductTypes
 
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
 
-class Item (json: JsValue) {
-  val sku: String = json("sku").as[String]
+class Item (json: JsValue, user: Users.User) {
+  var sku: String = json("sku").as[String]
 
-  val name: String = json("name").as[String]
+  var name: String = json("name").as[String]
 
-  val price: Double = {
+  var price: Double = {
     val url = "https://api.wegmans.io/products/" + sku + "/prices?api-version=2018-10-18&subscription-key=68527dbd17d345e18b45513c9a60782a"
     val data = Json.parse(scala.io.Source.fromURL(url).mkString)
-
-    data("store").head("price").as[Double]
+    val stores = Json.arr(data("stores"))
+    0
   }
+  var ingredients: List[String] = json("ingredients").as[String].split(",").toList
 
-  val ingredients: List[String] = json("ingredients").as[String].split(",").toList
+  var location: String = new String
 
-  val location: String = new String
-
-  val nutrients: List[Map[String, String]] = List()
+  var nutrients: List[Map[String, String]] = List()
 }
--- src/Users/Customer.scala
+++ src/Users/Customer.scala
@@ -2,5 +2,7 @@ package Users
 
 class Customer extends User {
   override def userType: String = "Customer"
+
+  override def storeNum: Int = 1
 }
 //commit bruh
