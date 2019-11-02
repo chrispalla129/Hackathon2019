34b8f1bb72ef791af441c581f1f4cfb8ee965878
jbreindl <jbreindl@buffalo.edu>
2019-11-02 17:57:09 -0400

    json intensifies

--- src/ProductTypes/Item.scala
+++ src/ProductTypes/Item.scala
@@ -1,13 +1,20 @@
 package ProductTypes
 
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsValue, Json}
 
-class Item(json: JsValue) {
-  val name: String = new String
+class Item (json: JsValue) {
+  val sku: String = json("sku").as[String]
 
-  val price: Double = new Double
+  val name: String = json("name").as[String]
 
-  val ingredients: List[String] = List()
+  val price: Double = {
+    val url = "https://api.wegmans.io/products/" + sku + "/prices?api-version=2018-10-18&subscription-key=68527dbd17d345e18b45513c9a60782a"
+    val data = Json.parse(scala.io.Source.fromURL(url).mkString)
+
+    data("store").head("price").as[Double]
+  }
+
+  val ingredients: List[String] = json("ingredients").as[String].split(",").toList
 
   val location: String = new String
 
