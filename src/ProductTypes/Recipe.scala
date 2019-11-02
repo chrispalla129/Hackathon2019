b7ca1b4c75fc76a3a86629466311878cf7d7d0d0
ygtron <truongph@buffalo.edu>
2019-11-02 17:30:17 -0400

    Recipe update 2

--- src/ProductTypes/Recipe.scala
+++ src/ProductTypes/Recipe.scala
@@ -8,8 +8,9 @@ import com.ibm.watson.discovery.v1.model.AddDocumentOptions
 import com.ibm.watson.discovery.v1.model.DocumentAccepted
 import java.io.ByteArrayInputStream
 import java.io.InputStream
+import Users._
 
-class Recipe {
+class Recipe(User: User) {
   val key: String ="_NFPz15p1P1hKI19A5cW7xx-rZ0BClaOBl6A_s4SEnJN"
   val site: String = "https://gateway.watsonplatform.net/discovery/api"
   val authenticator = new IamAuthenticator(key)
