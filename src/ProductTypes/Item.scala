5260f52b7c3d103ff1f3b3d1b79cbd00a81a6023
ygtron <truongph@buffalo.edu>
2019-11-03 00:46:12 -0400

    Testees

--- src/ProductTypes/Item.scala
+++ src/ProductTypes/Item.scala
@@ -3,32 +3,30 @@ package ProductTypes
 import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
 
 class Item (json: JsValue, user: Users.User) {
-  var sku: String = json("sku").as[String]
+  var sku: String = (json \ "sku").as[Int].toString
 
   var price: Double = {
     val url = "https://api.wegmans.io/products/" + sku + "/prices?api-version=2018-10-18&subscription-key=68527dbd17d345e18b45513c9a60782a"
     val data = Json.parse(scala.io.Source.fromURL(url).mkString)
     val stores = (data \ "stores").as[Array[JsValue]]
     for (store <- stores) if (store("store").as[Int] == user.storeNum) price = store("price").as[Double]
-    -2
+    -1
   }
 
-  var ingredients: Array[JsValue] = {
-    val url = "https://api.wegmans.io/products/" + sku + "/prices?api-version=2018-10-18&subscription-key=68527dbd17d345e18b45513c9a60782a"
-    val data = Json.parse(scala.io.Source.fromURL(url).mkString)
-    val ingredients = (data \ "ingredients").as[Array[JsValue]]
-    ingredients
+  var ingredients: String = {
+    val fin = (json \ "ingredients").as[Array[String]]
+    fin(0)
   }
 
   var location: String = {
     val url = "https://api.wegmans.io/products/" + sku + "/locations/" +
       user.storeNum.toString + "?api-version=2018-10-18&subscription-key=68527dbd17d345e18b45513c9a60782a"
     val data = Json.parse(scala.io.Source.fromURL(url).mkString)
-
-    if (data.as[Map[String, JsValue]].contains("error")) "Sorry! This item is not in stock"
+    val mapped = data.as[Map[String, JsValue]]
+    if (mapped.contains("error")) "Sorry! This item is not in stock"
     else {
-      val loc = data("locations")
-      var ret = "Aisle Name: " + loc("name")
+      val loc = mapped("locations")(0).as[Map[String, JsValue]]
+      var ret = "Aisle Name: " + loc("name").toString
       if (loc("aisleSide") != null) ret += " Aisle Side: " + loc("aisleSide").toString
       ret += " Shelf Number: " + loc("shelfNumber").toString
       ret
