ed0c5b1d311d98eaacb32c1bd04cce7f8cc69c29
ygtron <truongph@buffalo.edu>
2019-11-02 21:15:52 -0400

    Recipe update a roonie

--- src/DataProcess/Search.scala
+++ src/DataProcess/Search.scala
@@ -5,9 +5,10 @@ import play.api.libs.json.{JsValue, Json}
 
 object Search {
   //return List[Item]
-  def search(input: String) ={
-    val url = "https://api.wegmans.io/products/" + input + "&api-version=2018-10-18&subscription-key=da8f3095e1e94773add7ab4cb71eacc1"
-    val data = Json.parse(scala.io.Source.fromURL(url).mkString)
+  def searchProduct(input: String) ={
+    val input = "Milk"
+    val url = "https://api.wegmans.io/products/search?query="+ input +"&api-version=2018-10-18&subscription-key=da8f3095e1e94773add7ab4cb71eacc1"
+    val data = Json.parse(get(url))
     val list = (data \ "results")
     println(list)
 
