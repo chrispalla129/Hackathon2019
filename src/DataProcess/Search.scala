cd9ab71d9ba9f98ffed9e81a25d9ae2762e1669b
jbreindl <jbreindl@buffalo.edu>
2019-11-02 23:49:10 -0400

    fixed truong thingy

--- src/DataProcess/Search.scala
+++ src/DataProcess/Search.scala
@@ -6,26 +6,32 @@ import play.api.libs.json.{JsValue, Json}
 
 object Search {
   //return List[Item]
-  def searchProduct(input: String) ={
+  def searchProduct(input: String):List[Item] ={
     val input = "Milk"
     val url = "https://api.wegmans.io/products/search?query="+ input +"&api-version=2018-10-18&subscription-key=da8f3095e1e94773add7ab4cb71eacc1"
     val data = Json.parse(get(url))
     val list = (data \ "results").as[Array[JsValue]]
     val customer = new Customer
+    var ret: List[Item] = List()
 
     for(i<-list){
       val j = new Item(i, customer)
+      ret :+= j
     }
+    ret
   }
-  def searchRecipe(input: String) ={
+  def searchRecipe(input: String):List[Recipe] ={
     val url = "https://api.wegmans.io/meals/recipes/search?query="+ input +"&api-version=2018-10-18&results=100&page=1&subscription-key=da8f3095e1e94773add7ab4cb71eacc1"
     val data = Json.parse(get(url))
     val list = (data \ "results").as[Array[JsValue]]
     val customer = new Customer
+    var ret: List[Recipe] = List()
 
     for(i<-list){
       val j = new Recipe(customer, i)
+      ret :+= j
     }
+    ret
   }
   def get(url: String,
           requestMethod: String = "GET"): String =
