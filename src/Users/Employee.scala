1f38e41e1dd91f6ce8d415a05f28e41e7a4fefe5
Christian <chrispalla129@gmail.com>
2019-11-02 22:23:39 -0400

    spaghetti spaghetti give me the depresetti

--- src/Users/Employee.scala
+++ src/Users/Employee.scala
@@ -1,10 +1,12 @@
 package Users
 import ProductTypes.ShoppingList
 
+import scala.collection.mutable
+
 class Employee extends User{
   override def userType: String = "Employee"
 
   override def storeNum: Int = 90
 
-  override def shoppingLists: Map[String, ShoppingList] = Map()
+  override def shoppingLists: mutable.Map[String, ShoppingList] = mutable.Map()
 }
--- src/Users/User.scala
+++ src/Users/User.scala
@@ -2,12 +2,14 @@ package Users
 
 import ProductTypes.ShoppingList
 
+import scala.collection.mutable
+
 abstract class User {
   def userType:String
   def storeNum: Int
-  def shoppingLists: Map[String, ShoppingList]
+  def shoppingLists: mutable.Map[String, ShoppingList]
   def saveList(shopping: ShoppingList,  name: String): Unit = {
     shopping.name = name
-    this.shoppingLists += (shopping.name -> shopping)
+    this.shoppingLists(shopping.name) = shopping
   }
 }
